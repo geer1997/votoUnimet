4c75b657c20575d3185402d3a1b5c451f2bdaaa7
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2017-11-27 15:57:56 -0400

    Firebase Integration :top: :fire:
    We can make a Firebase Init now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
-
+import { Observable } from 'rxjs/Observable':
+ 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
