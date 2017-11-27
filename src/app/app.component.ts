468bec04cde7a39c3bfc72ea78722cd62ad8cd28
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2017-11-27 17:01:41 -0400

    Arregle angularfire para que usara el Real Time Database y no Firestore. Ademas intente empezar un beta con la autenticacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable':
- 
+import { AngularFireDatabase} from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  constructor(db: AngularFirestore) {
+  constructor(db: AngularFireDatabase) {
 
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'votoUnimet'), // imports firebase/app needed for everything
-    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireDatabaseModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
-  declarations: [ AppComponent, NavbarComponent, FooterComponent ],
+  declarations: [ AppComponent, NavbarComponent, FooterComponent, UserLoginComponent, UserProfileComponent ],
   providers:[],
   bootstrap: [ AppComponent ]
 })
